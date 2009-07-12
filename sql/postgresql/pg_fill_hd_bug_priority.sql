56c0d2dd2b79bee5c309cad0e38ece7c6f5e80f6
Rafał Kotusiewicz <r.kotusiewicz@gmail.com>
2009-07-12 18:02:18 +0000

    uporz??dkowana kwestia sql'a - na razie tylko instalacja bez inicjalizacji bazy, usuni??te niepotrzebne pliki

--- sql/postgresql/pg_fill_hd_bug_priority.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO hd_bug_priority(priority_id, priority_name, description, sort_order)
-    VALUES (1, 'normalny', '', 1);
-INSERT INTO hd_bug_priority(priority_id, priority_name, description, sort_order)
-    VALUES (2, 'wazny', '', 2);
-INSERT INTO hd_bug_priority(priority_id, priority_name, description, sort_order)
-    VALUES (3, 'bardzo wazny', '', 3);
-INSERT INTO hd_bug_priority(priority_id, priority_name, description, sort_order)
-    VALUES (4, 'krytyczny', '', 4);
