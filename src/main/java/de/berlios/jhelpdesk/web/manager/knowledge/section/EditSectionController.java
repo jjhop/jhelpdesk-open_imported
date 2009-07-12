1b0db711ba2dfae0a341553d92f7505aec27d925
Rafał Kotusiewicz <r.kotusiewicz@gmail.com>
2009-07-12 20:59:22 +0000

    Zmieniona nazwa metody w DAO save -> saveOrUpdate (zeby uniknac nieporozumien), za tym poszly zmiany w implementacji i kontrolerze, interfejs KnowledgeSectionDAO zosta?? dokladnie opatrzony dokumentacj??.

--- src/main/java/de/berlios/jhelpdesk/web/manager/knowledge/section/EditSectionController.java
+++ src/main/java/de/berlios/jhelpdesk/web/manager/knowledge/section/EditSectionController.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
@@ -29,38 +30,38 @@ import de.berlios.jhelpdesk.dao.KnowledgeSectionDAO;
 import de.berlios.jhelpdesk.model.KnowledgeSection;
 
 public class EditSectionController extends SimpleFormController {
-	
-	private static Log log = LogFactory.getLog(EditSectionController.class);
-	private KnowledgeSectionDAO sectionDAO;
 
-	@Override
-	protected Object formBackingObject(HttpServletRequest request) throws Exception {
-		KnowledgeSection section = null;
-		try {
-			section = sectionDAO.getById(
-					Long.parseLong(request.getParameter("sectionId")));
-		} catch (Exception ex) {
-			log.warn(ex);
-			section = new KnowledgeSection();
-		}
-		return section;
-	}
+    private static Log log = LogFactory.getLog(EditSectionController.class);
+    private KnowledgeSectionDAO sectionDAO;
 
-	@Override
-	protected ModelAndView onSubmit(HttpServletRequest request, 
-			HttpServletResponse response, Object command, BindException errors) 
-			throws Exception {
-		ModelAndView mav = new ModelAndView(
-			new RedirectView("/manage/knowledge/section/showAll.html", true));
-		KnowledgeSection section = (KnowledgeSection) command;
-		sectionDAO.save(section);
-		return mav;
-	}
+    @Override
+    protected Object formBackingObject(HttpServletRequest request) throws Exception {
+        KnowledgeSection section = null;
+        try {
+            section = sectionDAO.getById(
+                Long.parseLong(request.getParameter("sectionId")));
+        } catch (Exception ex) {
+            log.warn(ex);
+            section = new KnowledgeSection();
+        }
+        return section;
+    }
 
-	/**
-	 * @param sectionDAO the sectionDAO to set
-	 */
-	public void setSectionDAO(KnowledgeSectionDAO sectionDAO) {
-		this.sectionDAO = sectionDAO;
-	}
+    @Override
+    protected ModelAndView onSubmit(HttpServletRequest request,
+        HttpServletResponse response, Object command, BindException errors)
+        throws Exception {
+        ModelAndView mav = new ModelAndView(
+            new RedirectView("/manage/knowledge/section/showAll.html", true));
+        KnowledgeSection section = (KnowledgeSection) command;
+        sectionDAO.saveOrUpdate(section);
+        return mav;
+    }
+
+    /**
+     * @param sectionDAO the sectionDAO to set
+     */
+    public void setSectionDAO(KnowledgeSectionDAO sectionDAO) {
+        this.sectionDAO = sectionDAO;
+    }
 }
