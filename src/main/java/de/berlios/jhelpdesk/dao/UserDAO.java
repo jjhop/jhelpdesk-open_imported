85fad0de3bda7bd599b4b210203575a14b080e94
Rafał Kotusiewicz <r.kotusiewicz@gmail.com>
2009-07-12 18:45:41 +0000

    Dodane komentarze do metod w interfejsie oraz do samego interfejsu.

--- src/main/java/de/berlios/jhelpdesk/dao/UserDAO.java
+++ src/main/java/de/berlios/jhelpdesk/dao/UserDAO.java
@@ -22,19 +22,111 @@ import de.berlios.jhelpdesk.model.Role;
 import de.berlios.jhelpdesk.model.User;
 
 /**
- * Podstawowe DAO dla obiektów User.
+ * Podstawowe DAO dla obiektów User. Wykonuje wszystkie istotne operacje bazodanowe
+ * związane z użytkownikiem, czyli nieco ponad standardowy CRUD.
+ * 
  * @author jjhop
-*/
+ */
 public interface UserDAO {
-	List<User> getAllUserWithLastNameStartsWithLetter( String letter );
-	List<User> getSavioursWithLastNameStartsWithLetter( String letter );
-	List<User> getAllUser();
-	List<User> getByRole( Role role );
-	List<User> getSaviours();
-	User getById( Long id );
-	User getById( int id );
-	User getByLogin( String login );
-	boolean checkLoginAndPassw(String login, String passw);
-	void loginUser(String login, Date date);
-	void save( User user );
+
+    /**
+     * Zwraca listę użytkowników, których nazwisko zaczyna się na podaną literę.
+     * Jeśli żaden użytkownik nie zostanie odnaleziony zwracana jest pusta lista.
+     *
+     * @param letter pierwsza litera nazwiska szukanych użytkowników
+     * @return lista użytkowników, których nazwisko zaczyna się na podaną literę
+     *
+     * @see User
+     * @see java.util.Collections#EMPTY_LIST
+     */
+    List<User> getAllUserWithLastNameStartsWithLetter(String letter);
+
+    /**
+     * Zwraca listę użytkowników w roli {@link Role#BUGKILLER}, których nazwisko
+     * zaczyna się na podaną literę.
+     * 
+     * @param letter pierwsza litera nazwiska szukanych użytkowników
+     * @return lista użytkowników, których nazwisko zaczyna się na podaną literę
+     *
+     * @see User
+     * @see java.util.Collections#EMPTY_LIST
+     */
+    List<User> getSavioursWithLastNameStartsWithLetter(String letter);
+
+    /**
+     * Zwraca wszystkich użytkowników w systemie. Lista zawsze jest jest różna od {@code null}
+     * i zawiera przynajmniej jednego użytkownika.
+     *
+     * @return lista wszystkich użytkowników w systemie
+     *
+     * @see User
+     */
+    List<User> getAllUser();
+
+    /**
+     * Zwraca wszystkich użytkowników sytemu z podaną rolą. Jeśli nie zostanie znaleziony
+     * żadne użytkownik zwrócona zostanie pusta lista.
+     *
+     * @param role rola, jaką mają mieć szukani użytkownicy
+     * @return lista użytkowników systemu z podaną rolą
+     *
+     * @see User
+     * @see java.util.Collections#EMPTY_LIST
+     */
+    List<User> getByRole(Role role);
+
+    /**
+     * @deprecated należy używać {@link #getByRole(Role role)}
+     */
+     List<User> getSaviours();
+
+    /**
+     * Zwraca użytkownika o podanym identyfikatorze lub {@code null} jeśli
+     * nie zostanie odnaleziony.
+     *
+     * @param id identyfikator poszukiwanego użytkownika
+     * @return obiekt użytkownika o podanym identyfikatorze
+     *
+     * @see User
+     */
+    User getById(Long id);
+
+    /**
+     * Zwraca użytkownika o podanym loginie lub {@code null} jeśli
+     * nie zostanie odnaleziony.
+     *
+     * @param login login poszukiwanego użytkownika
+     * @return obiekt użytkownika o podanym identyfikatorze
+     */
+    User getByLogin(String login);
+
+    /**
+     * Sprawdza czy w systemie istnieje użytkownik z pasującą do podanych
+     * parą {@code login} oraz {@code hasło}. Zwraca {@code true} jeśli użytkownik
+     * zostanie odnaleziony i hasło pasuje oraz {@code false} w każdym innym wypadku.
+     * 
+     * @param login login użytkownika
+     * @param passw hasło użytkownika
+     * @return {@code true} jeśli odnajdzie użytkownika z podanym hasłem oraz {@code false}
+     *     w każdym innym wypadku
+     */
+    boolean checkLoginAndPassw(String login, String passw);
+
+    /**
+     * Loguje użytkownika w systemie. Zapisuje informacje o czasie logowania.
+     *
+     * @param login login uzytkownika (użytkownik o tym loginie musi istnieć w systemie)
+     * @param date data logowanie
+     */
+    void loginUser(String login, Date date);
+
+    /**
+     * Zapisuje dane nowego użytkownika lub uaktualnie dane istniejącego. Wybór akcji odbywa się
+     * na podstawie wartości identyfikatora ({@link User#getUserId()}. Jeśli jest różny od {@code null}
+     * to uaktualnie dane użytkownika o tym właśnie identyfikatorze w przeciwym wypadku zapisuje
+     * dane nowego użytkownika. Identyfikator w obiekcie zostaje uaktualniony o wartość nadaną w bazie.
+     *
+     * @param user obiekt użytkownika do zapisania lub uaktualnienia
+     */
+    void saveOrUpdate(User user);
 }
