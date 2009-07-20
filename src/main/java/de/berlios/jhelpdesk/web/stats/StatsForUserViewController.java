bff1d3a297626746af8761f6dfb4185a33d250cd
jjhop <jjhop@localhost>
2009-07-20 15:59:18 +0000

    Dodawanie artykulow wstepnie dziala. Poprawki w ArticleDAOJdbc, zmieniony sposob docierania do danych uzytkownika z sesji w ArticleController. Usuniety nieuzywany kod w StatsForUserViewController.

--- src/main/java/de/berlios/jhelpdesk/web/stats/StatsForUserViewController.java
+++ src/main/java/de/berlios/jhelpdesk/web/stats/StatsForUserViewController.java
@@ -15,17 +15,11 @@
  */
 package de.berlios.jhelpdesk.web.stats;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import de.berlios.jhelpdesk.dao.UserDAO;
-
 @Controller("statsMyViewCtrl")
 public class StatsForUserViewController {
-
-    @Autowired
-    private UserDAO hdUserDAO;
 
     @RequestMapping("/stats/myStats.html")
     public String handleRequest() {
