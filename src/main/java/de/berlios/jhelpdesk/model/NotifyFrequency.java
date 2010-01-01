cf3074b03b0d4c943e724191485fc477db8b2ae7
jjhop <jjhop@localhost>
2010-01-01 12:43:56 +0000

    Dodany enum (wraz z edytorem) do obs?ugi ustawie? powiadomie? o zdarzeniach.

---
+++ src/main/java/de/berlios/jhelpdesk/model/NotifyFrequency.java
@@ -0,0 +1,62 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright: (C) 2006 jHelpdesk Developers Team
+ */
+package de.berlios.jhelpdesk.model;
+
+/**
+ *
+ * @author jjhop
+ */
+public enum NotifyFrequency {
+
+    IMMEDIATLY(1, "IMMEDIATLY"),
+    DAILY(2, "DAILY"),
+    WEEKLY(3, "WEEKLY"),
+    MONTHLY(4, "MONTHLY"),
+    NEVER(5, "NEVER");
+    
+    private final int id;
+    private final String name;
+
+    private NotifyFrequency(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public static final NotifyFrequency fromInt(int i) {
+        switch(i) {
+            case 1:
+                return IMMEDIATLY;
+            case 2:
+                return DAILY;
+            case 3:
+                return WEEKLY;
+            case 4:
+                return MONTHLY;
+            case 5:
+                return NEVER;
+            default:
+                throw new RuntimeException("Nieznana wartość int dla NotifyFrequency");
+        }
+    }
+}
