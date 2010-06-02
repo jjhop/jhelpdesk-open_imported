53b339695ae5cb40eecee9be379d64c60780690e
jjhop <jjhop@localhost>
2010-06-02 02:34:24 +0000

    pliki js i tematy nieco uporz?dkowane

---
+++ src/main/webapp/js/tiny_init.js
@@ -0,0 +1,13 @@
+tinyMCE.init({
+    width : "420",
+    mode : "textareas",
+    editor_selector : "mceEditor",
+    theme : "advanced",
+    theme_advanced_buttons1 : "bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist",
+    theme_advanced_buttons2 : "",
+    theme_advanced_buttons3 : "",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_path : false,
+    extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]"
+});
