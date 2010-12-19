7d45fea66f6e31cb61aa81bda9818c01605eb6f0
Rafał Kotusiewicz <r.kotusiewicz@gmail.com>
2010-12-19 14:14:58 +0000

    Zaimplementowane zarz?dzanie preferencjami

--- src/main/java/de/berlios/jhelpdesk/web/preferences/DisplayListsEditController.java
+++ src/main/java/de/berlios/jhelpdesk/web/preferences/DisplayListsEditController.java
@@ -15,12 +15,18 @@
  */
 package de.berlios.jhelpdesk.web.preferences;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import de.berlios.jhelpdesk.web.form.DisplayListsForm;
+import de.berlios.jhelpdesk.dao.UserPreferencesDAO;
+import de.berlios.jhelpdesk.model.DisplayListsPreferences;
+import de.berlios.jhelpdesk.model.User;
 
 /**
  *
@@ -29,8 +35,42 @@ import de.berlios.jhelpdesk.web.form.DisplayListsForm;
 @Controller
 public class DisplayListsEditController {
 
+    @Autowired
+    private UserPreferencesDAO userPreferencesDAO;
+
     @RequestMapping(value = "/preferences/displayLists.html", method = RequestMethod.GET)
-    public String prepareForm(@ModelAttribute("preferences") DisplayListsForm preferences) {
+    public String prepareForm(@ModelAttribute("dlPrefs") DisplayListsPreferences preferences,
+                              HttpSession session, ModelMap map) {
+
+        User currentUser = (User) session.getAttribute("user");
+        DisplayListsPreferences dlPreferences = currentUser.getDlPreferences();
+        map.addAttribute("dlPrefs", dlPreferences != null
+                                            ? dlPreferences
+                                            : new DisplayListsPreferences());
         return "preferences/displayLists";
+    }
+
+    @RequestMapping(value = "/preferences/displayLists.html", method = RequestMethod.POST)
+    public String processForm(@ModelAttribute("dlPrefs") DisplayListsPreferences dlPreferences,
+                              HttpSession session, ModelMap map) {
+
+        User currentUser = (User) session.getAttribute("user");
+        if (isPrefsOwnedByUser(dlPreferences, currentUser)) {
+            dlPreferences.setUser(currentUser);
+            currentUser.setDlPreferences(dlPreferences);
+            this.userPreferencesDAO.save(dlPreferences);
+            session.setAttribute("user", currentUser);
+            map.addAttribute("preferences", dlPreferences);
+        }
+        return "preferences/displayLists";
+    }
+
+    private boolean isPrefsOwnedByUser(DisplayListsPreferences dlPrefs, User user) {
+        DisplayListsPreferences userDlPrefs = user.getDlPreferences();
+        Long lafId = dlPrefs.getId();
+        if (userDlPrefs == null || lafId == null) {
+            return false;
+        }
+        return lafId.equals(userDlPrefs.getId());
     }
 }
--- src/main/java/de/berlios/jhelpdesk/web/preferences/PersonalDataEditController.java
+++ src/main/java/de/berlios/jhelpdesk/web/preferences/PersonalDataEditController.java
@@ -15,12 +15,21 @@
  */
 package de.berlios.jhelpdesk.web.preferences;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import de.berlios.jhelpdesk.web.form.PersonalDataForm;
+import de.berlios.jhelpdesk.dao.UserDAO;
+import de.berlios.jhelpdesk.model.User;
 
 /**
  * 
@@ -29,8 +38,48 @@ import de.berlios.jhelpdesk.web.form.PersonalDataForm;
 @Controller
 public class PersonalDataEditController {
 
+    @Autowired
+    private UserDAO userDAO;
+
     @RequestMapping(value = "/preferences/personalData.html", method = RequestMethod.GET)
-    public String prepareForm(@ModelAttribute("preferences") PersonalDataForm personalDataForm) {
+    public String prepareForm(HttpSession session, ModelMap map) {
+        map.addAttribute("personalData", getUserFromSession(session));
         return "preferences/personalData";
+    }
+
+    @RequestMapping(value = "/preferences/personalData/change.html", method = RequestMethod.POST)
+    public String processPersonalDataChange(@ModelAttribute("personalData") User user,
+                                            BindingResult result) {
+        // 1. walidacje a jeśli jest ok to przepisujemy do usera z bazki i zapisujemy i zmykamy
+
+
+        return "preferences/personalData";
+    }
+
+    @RequestMapping(value = "/preferences/password/change.html", method = RequestMethod.POST)
+    public String processPasswordChange(@RequestParam("password") String password,
+                                        @RequestParam("repeated") String repeated,
+                                        HttpSession session, ModelMap map) {
+        User currentUser = getUserFromSession(session);
+        map.addAttribute("personalData", currentUser);
+
+        if (passwordsAreSameAndValid(password, repeated)) {
+            userDAO.updatePasswordAndSalt(currentUser, password);
+            map.addAttribute("message", "wyciagamy info z messagesów"); // UDAŁO SIĘ!
+        } else {
+            map.addAttribute("message", "nie udało się!!!!"); // PORAŻKA!
+        }
+        return "preferences/personalData";
+    }
+
+    private boolean passwordsAreSameAndValid(String p1, String p2) {
+        boolean containsNumber = StringUtils.containsAny(p1, "0123456789");
+        boolean containsSpecial = StringUtils.containsAny(p1, "!@#$%^&*()_+-=/*,.;:\\\"'`~'");
+
+        return (p1.length() < 6 && p1.equals(p2) && containsNumber && containsSpecial);
+    }
+
+    private User getUserFromSession(HttpSession session) {
+        return userDAO.getById(((User) session.getAttribute("user")).getUserId());
     }
 }
