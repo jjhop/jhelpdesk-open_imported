fe16d08579bf659e5862361e0decdafb685acf4c
Rafał Kotusiewicz <r.kotusiewicz@gmail.com>
2011-07-30 03:19:21 +0200

    Po?aczenie dwa pliki *.properties w jeden, DesktropController przeniesiony pakiet wy?ej, ma?e refaktoryzacji w konfiguracji springa

--- src/main/java/de/berlios/jhelpdesk/web/DesktopViewController.java
+++ src/main/java/de/berlios/jhelpdesk/web/DesktopViewController.java
@@ -13,7 +13,7 @@
  * 
  * Copyright: (C) 2006 jHelpdesk Developers Team
  */
-package de.berlios.jhelpdesk.web.desktop;
+package de.berlios.jhelpdesk.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
