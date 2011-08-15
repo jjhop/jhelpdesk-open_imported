0ec8c5afb502619860c27388b0bebf5863710496
rafal.kotusiewicz <r.kotusiewicz@gmail.com>
2011-08-15 23:11:43 +0200

    Zaimplementowana cz??ciowo funkcjonalno?? MailerService

--- src/main/java/de/berlios/jhelpdesk/mail/MailerService.java
+++ src/main/java/de/berlios/jhelpdesk/mail/MailerService.java
@@ -15,30 +15,123 @@
  */
 package de.berlios.jhelpdesk.mail;
 
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.context.Context;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import de.berlios.jhelpdesk.dao.TicketDAO;
+import de.berlios.jhelpdesk.model.Ticket;
 import de.berlios.jhelpdesk.model.User;
 
 @Component
 public class MailerService {
+
+    @Autowired
+    private TicketDAO ticketDAO;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private VelocityEngine velocityEngine;
 
     public void send(String recipient, String subject, String body) {
 
     }
 
     public void sendNotificationForTicketAssignEvent(Long ticketId) {
-        // maile wrzucamy do kolejki może jakoś? a stamtąd będziemy je
-        // jakimś mechanizmem pobierać i wysyłać...
-        System.out.println("[assign] Wyslałem mail zwiazany z ticketem #" + ticketId);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("jHelpDesk <moppee@mygreatcode.com>");
+        message.setSubject("Zgłoszenie  #" + ticketId + " zostało przypisane.");
+        try {
+            Ticket ticket = ticketDAO.getTicketById(ticketId);
+            Template template = this.velocityEngine.getTemplate("ticketAssignEvent.vm", "UTF-8");
+            Context context = new VelocityContext();
+            
+            context.put("ticket", ticket);
+            Writer writer = new StringWriter();
+            template.merge(context, writer);
+            message.setTo(ticket.getNotifier().getEmail());
+            message.setBcc("r.kotusiewicz@gmail.com");
+            message.setText(writer.toString());
+            this.mailSender.send(message);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void sendNotificationForNewSaviourForTicket(Long ticketId, User assigner, String comment) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("jHelpDesk <moppee@mygreatcode.com>");
+        message.setSubject("Zgłoszenie  #" + ticketId + " zostało zlecone Tobie.");
+        try {
+            Ticket ticket = ticketDAO.getTicketById(ticketId);
+            Template template = this.velocityEngine.getTemplate("ticketNotificationForNewSaviour.vm", "UTF-8");
+            Context context = new VelocityContext();
+
+            context.put("ticket", ticket);
+            context.put("assigner", assigner);
+            context.put("comment", comment);
+            Writer writer = new StringWriter();
+            template.merge(context, writer);
+            message.setTo(ticket.getNotifier().getEmail());
+            message.setBcc("r.kotusiewicz@gmail.com");
+            message.setText(writer.toString());
+            this.mailSender.send(message);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     public void sendNotificationForTicketRejectEvent(Long ticketId, User rejector, String comment) {
-        // j.w.
-        System.out.println("[reject] Wyslałem mail zwiazany z ticketem #" + ticketId);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("jHelpDesk <moppee@mygreatcode.com>");
+        message.setSubject("Zgłoszenie  #" + ticketId + " zostało odrzucone.");
+        try {
+            Ticket ticket = ticketDAO.getTicketById(ticketId);
+            Template template = this.velocityEngine.getTemplate("ticketRejectEvent.vm", "UTF-8");
+            Context context = new VelocityContext();
+            context.put("ticket", ticket);
+            context.put("comment", comment);
+            context.put("rejector", rejector);
+            Writer writer = new StringWriter();
+            template.merge(context, writer);
+            message.setTo(ticket.getNotifier().getEmail());
+            message.setBcc("r.kotusiewicz@gmail.com");
+            message.setText(writer.toString());
+            this.mailSender.send(message);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
-    public void sendNotificationForTicketCloseEvent(Long ticketId, User user, String comment) {
-        System.out.println("[close] Wyslałem mail zwiazany z ticketem #" + ticketId);
+    public void sendNotificationForTicketCloseEvent(Long ticketId) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("jHelpDesk <moppee@mygreatcode.com>");
+        message.setSubject("Zgłoszenie  #" + ticketId + " zostało odrzucone.");
+        try {
+            Ticket ticket = ticketDAO.getTicketById(ticketId);
+            Template template = this.velocityEngine.getTemplate("ticketRejectEvent.vm", "UTF-8");
+            Context context = new VelocityContext();
+            context.put("ticket", ticket);
+            Writer writer = new StringWriter();
+            template.merge(context, writer);
+            message.setTo(ticket.getNotifier().getEmail());
+            message.setBcc("r.kotusiewicz@gmail.com");
+            message.setText(writer.toString());
+            this.mailSender.send(message);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     public void sendNotificationForTicketResolveEvent(Long ticketId, User user, String comment) {
