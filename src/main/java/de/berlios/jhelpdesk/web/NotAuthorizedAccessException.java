28c641f6f7d0252065451c701fc6b24fdf8ca4e7
rafal.kotusiewicz <r.kotusiewicz@gmail.com>
2011-09-05 19:58:43 +0200

    Poprawiona kontrola zmiany kategorii i priorytetu zg?oszenia. zmiany w about.jsp

--- src/main/java/de/berlios/jhelpdesk/web/NotAuthorizedAccessException.java
+++ src/main/java/de/berlios/jhelpdesk/web/NotAuthorizedAccessException.java
@@ -16,17 +16,15 @@
 package de.berlios.jhelpdesk.web;
 
 /**
- *
  * @author jjhop
  */
 public class NotAuthorizedAccessException extends RuntimeException {
 
     private static final long serialVersionUID = -7396029049123879745L;
 
-    /**
-     *
-     * @param message
-     */
+    public NotAuthorizedAccessException() {
+    }
+
     public NotAuthorizedAccessException(String message) {
         super(message);
     }
